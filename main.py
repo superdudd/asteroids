16ff3e6d90f37b7f0ebd8c766b857adc23398e18
superdudd <superdudd@gmail.com>
2025-05-27 23:06:27 -0400

    C: fixed closing game error

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     while True:
-        screen.fill("black")
-        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            screen.fill("black")
+            pygame.display.flip()
 if __name__ == "__main__":
     main()
