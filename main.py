e61afe40a098e36811e152720bffc1b4837225af
superdudd <superdudd@gmail.com>
2025-05-27 23:46:20 -0400

    E: created player and draw it to screen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -12,13 +13,16 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen, "white", 2)
         pygame.display.flip()
+
         
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
