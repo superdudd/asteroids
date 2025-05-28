8ecc923980cf3bbf080084c736503978b9ecd83e
superdudd <superdudd@gmail.com>
2025-05-27 22:57:34 -0400

    A: setting up main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+import constants
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+if __name__ == "__main__":
+    main()
