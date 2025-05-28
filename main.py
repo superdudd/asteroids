c5144ac606ea01cee2e58a015e0eaa0fbb2c212a
superdudd <superdudd@gmail.com>
2025-05-27 23:04:49 -0400

    B: further refined main.py and added gitignore

--- main.py
+++ main.py
@@ -2,11 +2,16 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-import constants
+from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
 if __name__ == "__main__":
     main()
